41187075eb18a0a64085d357e0a1efaab96f16f2
Sup3r-Us3r <maydersonmello@gmail.com>
2022-01-10 10:15:13 -0300

    create photo component

---
+++ src/components/Photo/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {
+  Image,
+  Placeholder,
+  PlaceholderTitle,
+} from './styles';
+
+type Props = {
+  uri: string | null;
+};
+
+export const Photo = ({ uri }: Props) => {
+  if (uri) {
+    return (
+      <Image source={{ uri }} />
+    );
+  }
+
+  return (
+    <Placeholder>
+      <PlaceholderTitle>
+        Nenhuma foto{'\n'}carregada
+      </PlaceholderTitle>
+    </Placeholder>
+  );
+};
